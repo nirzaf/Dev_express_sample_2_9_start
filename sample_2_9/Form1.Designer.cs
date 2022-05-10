4a571e659fd4476defe200b679403f23cc72c95f
nirzaf <mfmfazrin1986@gmail.com>
2022-05-10 14:44:58 +0530

    Add project files.

---
+++ sample_2_9/Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace sample_2_9 {
+  partial class Form1 {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing) {
+      if (disposing && (components != null)) {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent() {
+      this.components = new System.ComponentModel.Container();
+      DevExpress.XtraBars.Ribbon.GalleryItemGroup galleryItemGroup1 = new DevExpress.XtraBars.Ribbon.GalleryItemGroup();
+      DevExpress.XtraBars.Ribbon.GalleryItem galleryItem1 = new DevExpress.XtraBars.Ribbon.GalleryItem();
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+      DevExpress.XtraBars.Ribbon.GalleryItem galleryItem2 = new DevExpress.XtraBars.Ribbon.GalleryItem();
+      DevExpress.XtraBars.Ribbon.GalleryItem galleryItem3 = new DevExpress.XtraBars.Ribbon.GalleryItem();
+      DevExpress.XtraBars.Ribbon.GalleryItemGroup galleryItemGroup2 = new DevExpress.XtraBars.Ribbon.GalleryItemGroup();
+      DevExpress.XtraBars.Ribbon.GalleryItem galleryItem4 = new DevExpress.XtraBars.Ribbon.GalleryItem();
+      DevExpress.XtraBars.Ribbon.GalleryItem galleryItem5 = new DevExpress.XtraBars.Ribbon.GalleryItem();
+      DevExpress.XtraBars.Ribbon.GalleryItem galleryItem6 = new DevExpress.XtraBars.Ribbon.GalleryItem();
+      DevExpress.XtraBars.Ribbon.GalleryItem galleryItem7 = new DevExpress.XtraBars.Ribbon.GalleryItem();
+      DevExpress.XtraBars.Ribbon.GalleryItemGroup galleryItemGroup3 = new DevExpress.XtraBars.Ribbon.GalleryItemGroup();
+      DevExpress.XtraBars.Ribbon.GalleryItem galleryItem8 = new DevExpress.XtraBars.Ribbon.GalleryItem();
+      DevExpress.Utils.SuperToolTip superToolTip1 = new DevExpress.Utils.SuperToolTip();
+      DevExpress.Utils.ToolTipTitleItem toolTipTitleItem1 = new DevExpress.Utils.ToolTipTitleItem();
+      DevExpress.Utils.ToolTipItem toolTipItem1 = new DevExpress.Utils.ToolTipItem();
+      DevExpress.Utils.ToolTipSeparatorItem toolTipSeparatorItem1 = new DevExpress.Utils.ToolTipSeparatorItem();
+      DevExpress.Utils.ToolTipItem toolTipItem2 = new DevExpress.Utils.ToolTipItem();
+      this.ribbonControl1 = new DevExpress.XtraBars.Ribbon.RibbonControl();
+      this.barToggleSwitchItem1 = new DevExpress.XtraBars.BarToggleSwitchItem();
+      this.barButtonItem1 = new DevExpress.XtraBars.BarButtonItem();
+      this.barStaticItem1 = new DevExpress.XtraBars.BarStaticItem();
+      this.barButtonItem2 = new DevExpress.XtraBars.BarButtonItem();
+      this.ribbonGalleryBarItem1 = new DevExpress.XtraBars.RibbonGalleryBarItem();
+      this.galleryDropDown1 = new DevExpress.XtraBars.Ribbon.GalleryDropDown(this.components);
+      this.barButtonItem3 = new DevExpress.XtraBars.BarButtonItem();
+      this.ribbonMiniToolbar1 = new DevExpress.XtraBars.Ribbon.RibbonMiniToolbar(this.components);
+      this.ribbonPage1 = new DevExpress.XtraBars.Ribbon.RibbonPage();
+      this.ribbonPageGroup1 = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
+      this.ribbonPageGroup3 = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
+      this.ribbonPage2 = new DevExpress.XtraBars.Ribbon.RibbonPage();
+      this.ribbonPageGroup2 = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
+      this.ribbonStatusBar1 = new DevExpress.XtraBars.Ribbon.RibbonStatusBar();
+      this.popupMenu1 = new DevExpress.XtraBars.PopupMenu(this.components);
+      ((System.ComponentModel.ISupportInitialize)(this.ribbonControl1)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.galleryDropDown1)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.popupMenu1)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // ribbonControl1
+      // 
+      this.ribbonControl1.CaptionBarItemLinks.Add(this.barToggleSwitchItem1);
+      this.ribbonControl1.ExpandCollapseItem.Id = 0;
+      this.ribbonControl1.Items.AddRange(new DevExpress.XtraBars.BarItem[] {
+            this.barToggleSwitchItem1,
+            this.ribbonControl1.ExpandCollapseItem,
+            this.ribbonControl1.SearchEditItem,
+            this.barButtonItem1,
+            this.barStaticItem1,
+            this.barButtonItem2,
+            this.ribbonGalleryBarItem1,
+            this.barButtonItem3});
+      this.ribbonControl1.Location = new System.Drawing.Point(0, 0);
+      this.ribbonControl1.MaxItemId = 7;
+      this.ribbonControl1.MiniToolbars.Add(this.ribbonMiniToolbar1);
+      this.ribbonControl1.Name = "ribbonControl1";
+      this.ribbonControl1.PageHeaderItemLinks.Add(this.barButtonItem2);
+      this.ribbonControl1.PageHeaderItemLinks.Add(this.barStaticItem1);
+      this.ribbonControl1.Pages.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPage[] {
+            this.ribbonPage1,
+            this.ribbonPage2});
+      this.ribbonControl1.QuickToolbarItemLinks.Add(this.barToggleSwitchItem1);
+      this.ribbonControl1.RibbonStyle = DevExpress.XtraBars.Ribbon.RibbonControlStyle.Office2019;
+      this.ribbonControl1.Size = new System.Drawing.Size(862, 158);
+      this.ribbonControl1.StatusBar = this.ribbonStatusBar1;
+      // 
+      // barToggleSwitchItem1
+      // 
+      this.barToggleSwitchItem1.Caption = "barToggleSwitchItem1";
+      this.barToggleSwitchItem1.Id = 4;
+      this.barToggleSwitchItem1.Name = "barToggleSwitchItem1";
+      // 
+      // barButtonItem1
+      // 
+      this.barButtonItem1.Caption = "barButtonItem1";
+      this.barButtonItem1.Id = 1;
+      this.barButtonItem1.Name = "barButtonItem1";
+      // 
+      // barStaticItem1
+      // 
+      this.barStaticItem1.Caption = "barStaticItem1";
+      this.barStaticItem1.Id = 2;
+      this.barStaticItem1.Name = "barStaticItem1";
+      // 
+      // barButtonItem2
+      // 
+      this.barButtonItem2.Caption = "barButtonItem2";
+      this.barButtonItem2.Id = 3;
+      this.barButtonItem2.Name = "barButtonItem2";
+      // 
+      // ribbonGalleryBarItem1
+      // 
+      this.ribbonGalleryBarItem1.Caption = "ribbonGalleryBarItem1";
+      // 
+      // 
+      // 
+      galleryItemGroup1.Caption = "Alignment";
+      galleryItem1.Caption = "Top";
+      galleryItem1.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("resource.SvgImage")));
+      galleryItem2.Caption = "Middle";
+      galleryItem2.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("resource.SvgImage1")));
+      galleryItem3.Caption = "Bottom";
+      galleryItem3.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("resource.SvgImage2")));
+      galleryItemGroup1.Items.AddRange(new DevExpress.XtraBars.Ribbon.GalleryItem[] {
+            galleryItem1,
+            galleryItem2,
+            galleryItem3});
+      galleryItemGroup2.Caption = "Text alignment";
+      galleryItem4.Caption = "Left";
+      galleryItem4.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("resource.SvgImage3")));
+      galleryItem5.Caption = "Center";
+      galleryItem5.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("resource.SvgImage4")));
+      galleryItem6.Caption = "Right";
+      galleryItem6.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("resource.SvgImage5")));
+      galleryItem7.Caption = "Block";
+      galleryItem7.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("resource.SvgImage6")));
+      galleryItemGroup2.Items.AddRange(new DevExpress.XtraBars.Ribbon.GalleryItem[] {
+            galleryItem4,
+            galleryItem5,
+            galleryItem6,
+            galleryItem7});
+      this.ribbonGalleryBarItem1.Gallery.Groups.AddRange(new DevExpress.XtraBars.Ribbon.GalleryItemGroup[] {
+            galleryItemGroup1,
+            galleryItemGroup2});
+      this.ribbonGalleryBarItem1.Gallery.ShowItemText = true;
+      this.ribbonGalleryBarItem1.GalleryDropDown = this.galleryDropDown1;
+      this.ribbonGalleryBarItem1.Id = 5;
+      this.ribbonGalleryBarItem1.Name = "ribbonGalleryBarItem1";
+      // 
+      // galleryDropDown1
+      // 
+      // 
+      // 
+      // 
+      galleryItemGroup3.Caption = "Group1";
+      galleryItem8.Caption = "Drop-down gallery item";
+      galleryItemGroup3.Items.AddRange(new DevExpress.XtraBars.Ribbon.GalleryItem[] {
+            galleryItem8});
+      this.galleryDropDown1.Gallery.Groups.AddRange(new DevExpress.XtraBars.Ribbon.GalleryItemGroup[] {
+            galleryItemGroup3});
+      this.galleryDropDown1.Gallery.ShowItemText = true;
+      this.galleryDropDown1.ItemLinks.Add(this.barButtonItem1);
+      this.galleryDropDown1.ItemLinks.Add(this.barToggleSwitchItem1);
+      this.galleryDropDown1.Name = "galleryDropDown1";
+      this.galleryDropDown1.Ribbon = this.ribbonControl1;
+      // 
+      // barButtonItem3
+      // 
+      this.barButtonItem3.Caption = "barButtonItem3";
+      this.barButtonItem3.Id = 6;
+      this.barButtonItem3.Name = "barButtonItem3";
+      // 
+      // ribbonMiniToolbar1
+      // 
+      this.ribbonMiniToolbar1.ItemLinks.Add(this.barButtonItem3);
+      this.ribbonMiniToolbar1.ParentControl = this;
+      // 
+      // ribbonPage1
+      // 
+      this.ribbonPage1.Groups.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPageGroup[] {
+            this.ribbonPageGroup1,
+            this.ribbonPageGroup3});
+      this.ribbonPage1.Name = "ribbonPage1";
+      this.ribbonPage1.Text = "ribbonPage1";
+      // 
+      // ribbonPageGroup1
+      // 
+      this.ribbonPageGroup1.ItemLinks.Add(this.barToggleSwitchItem1);
+      this.ribbonPageGroup1.ItemLinks.Add(this.barButtonItem1);
+      this.ribbonPageGroup1.ItemLinks.Add(this.barButtonItem2, "BX");
+      this.ribbonPageGroup1.Name = "ribbonPageGroup1";
+      this.ribbonPageGroup1.Text = "ribbonPageGroup1";
+      // 
+      // ribbonPageGroup3
+      // 
+      this.ribbonPageGroup3.CaptionButtonVisible = DevExpress.Utils.DefaultBoolean.True;
+      this.ribbonPageGroup3.ItemLinks.Add(this.ribbonGalleryBarItem1);
+      this.ribbonPageGroup3.Name = "ribbonPageGroup3";
+      toolTipTitleItem1.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("resource.SvgImage7")));
+      toolTipTitleItem1.Text = "This is the Ribbon Gallery";
+      toolTipItem1.Text = "Galleries can display options or commands for quick selection.";
+      toolTipItem2.Text = "They can also be useful for elements that require visual identification.";
+      superToolTip1.Items.Add(toolTipTitleItem1);
+      superToolTip1.Items.Add(toolTipItem1);
+      superToolTip1.Items.Add(toolTipSeparatorItem1);
+      superToolTip1.Items.Add(toolTipItem2);
+      this.ribbonPageGroup3.SuperTip = superToolTip1;
+      this.ribbonPageGroup3.Text = "Gallery";
+      // 
+      // ribbonPage2
+      // 
+      this.ribbonPage2.Groups.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPageGroup[] {
+            this.ribbonPageGroup2});
+      this.ribbonPage2.Name = "ribbonPage2";
+      this.ribbonPage2.Text = "ribbonPage2";
+      // 
+      // ribbonPageGroup2
+      // 
+      this.ribbonPageGroup2.ItemLinks.Add(this.barToggleSwitchItem1);
+      this.ribbonPageGroup2.Name = "ribbonPageGroup2";
+      this.ribbonPageGroup2.Text = "ribbonPageGroup2";
+      // 
+      // ribbonStatusBar1
+      // 
+      this.ribbonStatusBar1.ItemLinks.Add(this.barStaticItem1);
+      this.ribbonStatusBar1.Location = new System.Drawing.Point(0, 491);
+      this.ribbonStatusBar1.Name = "ribbonStatusBar1";
+      this.ribbonStatusBar1.Ribbon = this.ribbonControl1;
+      this.ribbonStatusBar1.Size = new System.Drawing.Size(862, 22);
+      // 
+      // popupMenu1
+      // 
+      this.popupMenu1.ItemLinks.Add(this.barToggleSwitchItem1);
+      this.popupMenu1.ItemLinks.Add(this.barButtonItem1);
+      this.popupMenu1.Name = "popupMenu1";
+      this.popupMenu1.Ribbon = this.ribbonControl1;
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(862, 513);
+      this.Controls.Add(this.ribbonStatusBar1);
+      this.Controls.Add(this.ribbonControl1);
+      this.Name = "Form1";
+      this.Ribbon = this.ribbonControl1;
+      this.StatusBar = this.ribbonStatusBar1;
+      this.Text = "Form1";
+      ((System.ComponentModel.ISupportInitialize)(this.ribbonControl1)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.galleryDropDown1)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.popupMenu1)).EndInit();
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private DevExpress.XtraBars.Ribbon.RibbonControl ribbonControl1;
+    private DevExpress.XtraBars.Ribbon.RibbonPage ribbonPage1;
+    private DevExpress.XtraBars.Ribbon.RibbonPageGroup ribbonPageGroup1;
+    private DevExpress.XtraBars.Ribbon.RibbonStatusBar ribbonStatusBar1;
+    private DevExpress.XtraBars.BarButtonItem barButtonItem1;
+    private DevExpress.XtraBars.BarStaticItem barStaticItem1;
+    private DevExpress.XtraBars.BarButtonItem barButtonItem2;
+    private DevExpress.XtraBars.BarToggleSwitchItem barToggleSwitchItem1;
+    private DevExpress.XtraBars.RibbonGalleryBarItem ribbonGalleryBarItem1;
+    private DevExpress.XtraBars.Ribbon.RibbonPageGroup ribbonPageGroup3;
+    private DevExpress.XtraBars.Ribbon.RibbonPage ribbonPage2;
+    private DevExpress.XtraBars.Ribbon.RibbonPageGroup ribbonPageGroup2;
+    private DevExpress.XtraBars.Ribbon.GalleryDropDown galleryDropDown1;
+    private DevExpress.XtraBars.BarButtonItem barButtonItem3;
+    private DevExpress.XtraBars.Ribbon.RibbonMiniToolbar ribbonMiniToolbar1;
+    private DevExpress.XtraBars.PopupMenu popupMenu1;
+  }
+}
+
